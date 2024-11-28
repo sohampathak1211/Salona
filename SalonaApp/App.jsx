566199d84d9a4a3b60c48868736a4cb95bd462d0
Soham Pathak <pathaksoham2003@gmail.com>
2024-11-28 18:32:29 +0530

    Added code

---
+++ SalonaApp/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import tw from 'twrnc';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, backgroundColor: 'red'}}>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
